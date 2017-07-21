21e2ca06cae5be6bb864445af4a5b426e4d226af
Howard Brown <hbrown951@gmail.com>
2017-07-21 09:04:56 -0600

    Add files via upload

---
+++ HB_Project02_EC/HB_Project02_v2/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace HB_Project02_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.piratesTextBox = new System.Windows.Forms.TextBox();
+            this.coinsTextBox = new System.Windows.Forms.TextBox();
+            this.jacksTextBox = new System.Windows.Forms.TextBox();
+            this.matesTextBox = new System.Windows.Forms.TextBox();
+            this.crewTextBox = new System.Windows.Forms.TextBox();
+            this.fundTextBox = new System.Windows.Forms.TextBox();
+            this.splitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.celebrationTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.crewTotalTextBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // piratesTextBox
+            // 
+            this.piratesTextBox.Location = new System.Drawing.Point(212, 120);
+            this.piratesTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.piratesTextBox.Name = "piratesTextBox";
+            this.piratesTextBox.Size = new System.Drawing.Size(96, 20);
+            this.piratesTextBox.TabIndex = 0;
+            // 
+            // coinsTextBox
+            // 
+            this.coinsTextBox.Location = new System.Drawing.Point(212, 149);
+            this.coinsTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.coinsTextBox.Name = "coinsTextBox";
+            this.coinsTextBox.Size = new System.Drawing.Size(96, 20);
+            this.coinsTextBox.TabIndex = 1;
+            // 
+            // jacksTextBox
+            // 
+            this.jacksTextBox.Location = new System.Drawing.Point(212, 222);
+            this.jacksTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.jacksTextBox.Name = "jacksTextBox";
+            this.jacksTextBox.Size = new System.Drawing.Size(96, 20);
+            this.jacksTextBox.TabIndex = 2;
+            // 
+            // matesTextBox
+            // 
+            this.matesTextBox.Location = new System.Drawing.Point(212, 249);
+            this.matesTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.matesTextBox.Name = "matesTextBox";
+            this.matesTextBox.Size = new System.Drawing.Size(96, 20);
+            this.matesTextBox.TabIndex = 3;
+            // 
+            // crewTextBox
+            // 
+            this.crewTextBox.Location = new System.Drawing.Point(212, 276);
+            this.crewTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.crewTextBox.Name = "crewTextBox";
+            this.crewTextBox.Size = new System.Drawing.Size(96, 20);
+            this.crewTextBox.TabIndex = 4;
+            // 
+            // fundTextBox
+            // 
+            this.fundTextBox.Location = new System.Drawing.Point(212, 301);
+            this.fundTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.fundTextBox.Name = "fundTextBox";
+            this.fundTextBox.Size = new System.Drawing.Size(96, 20);
+            this.fundTextBox.TabIndex = 5;
+            // 
+            // splitButton
+            // 
+            this.splitButton.Location = new System.Drawing.Point(212, 182);
+            this.splitButton.Margin = new System.Windows.Forms.Padding(2);
+            this.splitButton.Name = "splitButton";
+            this.splitButton.Size = new System.Drawing.Size(94, 23);
+            this.splitButton.TabIndex = 6;
+            this.splitButton.Text = "Split the Gold";
+            this.splitButton.UseVisualStyleBackColor = true;
+            this.splitButton.Click += new System.EventHandler(this.splitButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(58, 123);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Number of Pirates:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(58, 152);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Number of Gold Coins:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(57, 224);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Jack\'s Share:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(56, 250);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Mate\'s Share:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(54, 278);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Crew Member\'s Share:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(8, 304);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(200, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Pirate\'s Benevolent Fund:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(3, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(440, 24);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 22);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HB_Project02_v2.Properties.Resources.jolly_roger_icon;
+            this.pictureBox1.Location = new System.Drawing.Point(170, 334);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(170, 136);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // celebrationTextBox
+            // 
+            this.celebrationTextBox.Location = new System.Drawing.Point(212, 503);
+            this.celebrationTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.celebrationTextBox.Name = "celebrationTextBox";
+            this.celebrationTextBox.Size = new System.Drawing.Size(96, 20);
+            this.celebrationTextBox.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(57, 505);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(150, 16);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Celebration Cost:";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // crewTotalTextBox
+            // 
+            this.crewTotalTextBox.Location = new System.Drawing.Point(212, 537);
+            this.crewTotalTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.crewTotalTextBox.Name = "crewTotalTextBox";
+            this.crewTotalTextBox.Size = new System.Drawing.Size(96, 20);
+            this.crewTotalTextBox.TabIndex = 2;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(57, 539);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(150, 16);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Total Crew Payout:";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(210, 573);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(96, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(55, 575);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(150, 16);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Audit:";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(440, 696);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.splitButton);
+            this.Controls.Add(this.fundTextBox);
+            this.Controls.Add(this.crewTextBox);
+            this.Controls.Add(this.matesTextBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.crewTotalTextBox);
+            this.Controls.Add(this.celebrationTextBox);
+            this.Controls.Add(this.jacksTextBox);
+            this.Controls.Add(this.coinsTextBox);
+            this.Controls.Add(this.piratesTextBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Quartermaster Calculator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox piratesTextBox;
+        private System.Windows.Forms.TextBox coinsTextBox;
+        private System.Windows.Forms.TextBox jacksTextBox;
+        private System.Windows.Forms.TextBox matesTextBox;
+        private System.Windows.Forms.TextBox crewTextBox;
+        private System.Windows.Forms.TextBox fundTextBox;
+        private System.Windows.Forms.Button splitButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox celebrationTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox crewTotalTextBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
