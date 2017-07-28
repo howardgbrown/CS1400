d7efd4e4d20d91e8c8278b439a74b213ddf06f03
Howard Brown <hbrown951@gmail.com>
2017-07-28 08:51:00 -0600

    Add files via upload

---
+++ HB_Lab18/HB_Lab18/Program.cs
@@ -0,0 +1,57 @@
+﻿/* File Prolouge
+   Name: Howard Brown
+   Project: Lab 18
+   Date: 27 July 2017
+
+   I declare that the following source code was written soley by me.
+   I understand that copying any source code, in whole or in part,
+   constitutes cheating, and that I will receive a zero on this project
+   if I am found in violation of this policy.
+*/
+
+using System;
+
+namespace HB_Lab18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //do loop asking "Would you like to roll the dice?"
+            //while the answer is not "y" or "n"
+            //if answer is not "y" or "n"
+            //"This is not a valid response. Try again"
+
+            //do loop while the answer is not "y" or "n"
+
+            //for loop
+            //while the answer is not "y" or "n"
+            //random number generator
+            //random number generator
+
+            //if statement dice1 and dice2 = const boxcar
+            //"You rolled Box cars"
+
+            //else if statement dice1 and dice2 = constant snake eyes
+            //"You roll snake eyes"
+
+            //else statement
+            //"You rolled dice1 and dice2"
+
+            //do loop while the answer is not "y" or "n"
+            //Would you like to roll again?
+            //Method ReadLine will get the answer
+
+            //if statement the answer is not "y" or "n"
+            //Invalid Response message
+
+            //if statement answer is no
+            //Press any key to end
+
+            Console.WriteLine("This was just Psuedo code. Nothing to see here.");
+            Console.WriteLine("Press any key to end this program.");
+            Console.ReadKey();
+        }
+    }
+}
