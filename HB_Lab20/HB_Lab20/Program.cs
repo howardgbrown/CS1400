a2283a0360d33ed1556f6391a7e13be64cf40550
Howard Brown <hbrown951@gmail.com>
2017-08-02 18:04:47 -0600

    Add files via upload

---
+++ HB_Lab20/HB_Lab20/Program.cs
@@ -0,0 +1,52 @@
+﻿// File Prologue
+// Name: Howard Brown
+// CS 1400 - 602
+// Project: Lab 20 Passing Parameters by value and by reference
+// Date: 31 July 2017
+
+//I declare that the following source code was written soley by me.
+//I understand that copying any source code, in whole or in part,
+//constitutes cheating, and that I will receive a zero on this project
+//if I am found in violation of this policy.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HB_Lab20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare some variables to test the program
+            int number1 = 10;
+            int number2 = 15;
+
+            // call the Swap method and output the result
+            Swap(ref number1, ref number2);
+            Console.WriteLine("number1 = {0}", number1);
+            Console.WriteLine("number2 = {0}", number2);
+
+            Console.ReadKey(true);
+        }
+
+        // The Swap method
+        // Purpose: To interchange the value of two variables
+        // Parameters: two integers
+        // Returns: None
+        // Pre-conditions: None
+        // Post-conditions: None
+        // Side Effect: the values of the 2 integers are swapped
+
+        static void Swap(ref int n1, ref int n2)
+        {
+            int temp = n1;
+            n1 = n2;
+            n2 = temp;
+
+        }
+    }
+}
---
+++ HB_Project07/HB_Project07/Program.cs
@@ -0,0 +1,61 @@
+﻿/* File Prolouge
+   Name: Howard Brown
+   Project: Project 7
+   Date: 31 July 2017
+
+   I declare that the following source code was written soley by me.
+   I understand that copying any source code, in whole or in part,
+   constitutes cheating, and that I will receive a zero on this project
+   if I am found in violation of this policy.
+*/
+using System;
+
+
+namespace HB_Project07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare constant for Cages to equal 500
+            const int CAGES = 500;
+
+            // declare variables for month, adult rabbit pairs, baby rabbit pairs and total pairs
+            int month = 1;
+            int adults = 1;
+            int babies = 0;
+            int total = 1;
+            
+            // makes the background color white and the text blue
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            // write out "Table of rabbit population, in pairs."
+            Console.WriteLine("Table of rabbit population, in pairs.\n");
+            // write out Month - Adults - Babies - Total
+            Console.WriteLine($"Month\tAdults\tBabies\tTotal");
+            // write out the corresponding values for Month, Adults
+            // Babies and Total.
+            Console.WriteLine($"{month}\t{adults}\t{babies}\t{total}");
+
+            // loop to verify the value of Total to test if there
+            // are enough cages.
+            // a = b, b = c, c = a + b
+            //  increment months to add 1 month per line
+            do
+            {
+                babies = adults;
+                adults = total;
+                total = adults + babies;
+                month = ++month;
+
+                Console.WriteLine($"{month}\t{adults}\t{babies}\t{total}");
+                } while (total <= CAGES);
+            
+            // Write "You will run out of cages in the *month* month."
+            Console.WriteLine($"\nYou will run out of cages in the {month} month.");
+            Console.ReadKey(true);
+        }
+    }
+}
